02dfd77aa9485c089ae31f58d6d8285905fa7061
Slingshot <Centrica@static-71.48.99.14-tataidc.co.in>
2013-11-04 10:47:40 +0530

    Appdelegate2 comitted

---
+++ AppDelegate1.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BGBApp
+//
+//  Created by MobileCOE-iMac8 on 01/03/13.
+//  Copyright (c) 2013 BGBApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong,nonatomic) UINavigationController *mainNavigationController;
+
+@property (strong, nonatomic) ViewController *viewController;
+-(void) moveToRootView;
+@end
